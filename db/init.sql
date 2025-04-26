7d7b0a48ea33bc5d68f078ff1c356c09edefaa98
Ivan K <ivan.koval.andr@gmail.com>
2025-04-26 07:42:04 +0300

    create tables(users, orders, categories, products)

--- db/init.sql
+++ db/init.sql
@@ -1,18 +1,40 @@
-CREATE TABLE IF NOT EXISTS products (
-                                        id SERIAL PRIMARY KEY,
-                                        name VARCHAR(255),
-    price NUMERIC
-    );
+CREATE TABLE users (
+                       id UUID PRIMARY KEY,
+                       email TEXT UNIQUE NOT NULL,
+                       password_hash TEXT NOT NULL,
+                       name TEXT NOT NULL,
+                       role_id INT REFERENCES roles(id),
+                       created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE categories (
+                            id UUID PRIMARY KEY,
+                            name TEXT NOT NULL,
+                            created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE orders (
+                        id UUID PRIMARY KEY,
+                        user_id UUID NOT NULL,
+                        total_price DECIMAL NOT NULL,
+                        delivery_price DECIMAL NOT NULL,
+                        currency TEXT NOT NULL,
+                        status TEXT NOT NULL,
+                        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE products (
+                          id UUID PRIMARY KEY,
+                          name TEXT NOT NULL,
+                          description TEXT,
+                          price DECIMAL NOT NULL,
+                          category_id UUID REFERENCES categories(id),
+                          created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
 INSERT INTO products (name, price) VALUES
                                        ('Go Book', 29.99),
                                        ('Docker Stickers', 9.90);
-
-CREATE TABLE IF NOT EXISTS orders (
-                                      id SERIAL PRIMARY KEY,
-                                      product_id INT REFERENCES products(id),
-    quantity INT
-    );
 
 INSERT INTO orders (product_id, quantity) VALUES
                                               (1, 3),
