12d908b325c06144a7cb20a8c0e84a32c8df5092
Ivan K <ivan.koval.andr@gmail.com>
2025-05-16 10:28:49 +0300

    init commit

--- catalog/main.go
+++ catalog/main.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
+	"github.com/gorilla/mux"
 	"log"
+	"net/http"
 
-	_ "github.com/lib/pq"
+	"catalog/handlers"
 )
 
 func main() {
-	connStr := "host=db port=5432 user=myuser password=mypassword dbname=mydb sslmode=disable"
+	r := mux.NewRouter()
 
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal("Ошибка подключения:", err)
-	}
-	defer db.Close()
+	// Эндпоинты
+	r.HandleFunc("/products", handlers.GetAllProducts).Methods("GET")
+	r.HandleFunc("/products/{id}", handlers.GetProductByID).Methods("GET")
+	r.HandleFunc("/products", handlers.CreateProduct).Methods("POST")
+	r.HandleFunc("/products/{id}", handlers.UpdateProduct).Methods("PUT")
+	r.HandleFunc("/products/{id}", handlers.DeleteProduct).Methods("DELETE")
 
-	err = db.Ping()
-	if err != nil {
-		log.Fatal("PostgreSQL не отвечает:", err)
-	}
-
-	fmt.Println("Успешно подключились к базе данных!")
+	log.Println("Catalog service running on port 8081")
+	http.ListenAndServe(":8081", r)
 }
--- db/init.sql
+++ db/init.sql
@@ -1,11 +1,11 @@
-CREATE TABLE users (
-                       id UUID PRIMARY KEY,
-                       email TEXT UNIQUE NOT NULL,
-                       password_hash TEXT NOT NULL,
-                       name TEXT NOT NULL,
-                       role_id INT REFERENCES roles(id),
-                       created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+-- CREATE TABLE users (
+--                        id UUID PRIMARY KEY,
+--                        email TEXT UNIQUE NOT NULL,
+--                        password_hash TEXT NOT NULL,
+--                        name TEXT NOT NULL,
+--                        role_id INT REFERENCES roles(id),
+--                        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+-- );
 
 CREATE TABLE categories (
                             id UUID PRIMARY KEY,
@@ -24,18 +24,18 @@ CREATE TABLE orders (
 );
 
 CREATE TABLE products (
-                          id UUID PRIMARY KEY,
-                          name TEXT NOT NULL,
+                          id SERIAL PRIMARY KEY,
+                          name VARCHAR(100) NOT NULL,
                           description TEXT,
-                          price DECIMAL NOT NULL,
-                          category_id UUID REFERENCES categories(id),
-                          created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+                          price NUMERIC(10, 2) NOT NULL,
+                          category_id INTEGER,
+                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO products (name, price) VALUES
                                        ('Go Book', 29.99),
                                        ('Docker Stickers', 9.90);
 
-INSERT INTO orders (product_id, quantity) VALUES
-                                              (1, 3),
-                                              (2, 5);
+-- INSERT INTO orders (product_id, quantity) VALUES
+--                                               (1, 3),
+--                                               (2, 5);
