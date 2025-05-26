d9f48c777e734665a7f6bd80b42f6031cb030d44
Ivan K <ivan.koval.andr@gmail.com>
2025-05-27 01:35:09 +0300

    change main file(authService and backgroundWorkerService)

--- authService/main.go
+++ authService/main.go
@@ -1,1 +1,17 @@
 package authService
+
+import (
+	"log"
+	"net/http"
+	"reviews/routes"
+)
+
+func main() {
+	//router := routes.InitRoutes()
+
+	log.Println("Catalog Service running on port 8084")
+	err := http.ListenAndServe(":8084", router)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- backgroundWorkerService/main.go
+++ backgroundWorkerService/main.go
@@ -1,1 +1,16 @@
 package backgroundWorkerService
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	//router := routes.InitRoutes()
+
+	log.Println("Catalog Service running on port 8085")
+	err := http.ListenAndServe(":8085", router)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
